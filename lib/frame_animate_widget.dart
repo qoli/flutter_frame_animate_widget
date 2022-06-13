4174a1e9018d1fc0b29aa722fb63e9633311162a
Ronnie W <llqoli@gmail.com>
2022-06-13 10:57:28 +0800

    修改為單次播放模式

--- lib/frame_animate_widget.dart
+++ lib/frame_animate_widget.dart
@@ -23,6 +23,8 @@ class FrameAnimationImageState extends State<FrameAnimationImage> with SingleTic
   AnimationController _controller;
   int interval = 200;
 
+  AnimationStatus _status;
+
   @override
   void initState() {
     super.initState();
@@ -36,9 +38,13 @@ class FrameAnimationImageState extends State<FrameAnimationImage> with SingleTic
     // 启动动画controller
     _controller = new AnimationController(duration: Duration(milliseconds: maxTime), vsync: this);
     _controller.addStatusListener((AnimationStatus status) {
-      if (status == AnimationStatus.completed) {
-        _controller.forward(from: 0.0); // 完成后重新开始
-      }
+      // if (status == AnimationStatus.completed) {
+      // _controller.forward(from: 0.0); // 完成后重新开始
+      // }
+
+      setState(() {
+        _status = status;
+      });
     });
 
     _animation = new Tween<double>(begin: 0, end: imageCount.toDouble()).animate(_controller)
@@ -81,22 +87,37 @@ class FrameAnimationImageState extends State<FrameAnimationImage> with SingleTic
     int ix = _animation.value.floor() % widget._assetList.length;
 
     List<Widget> images = [];
-    // 把所有图片都加载进内容，否则每一帧加载时会卡顿
-    for (int i = 0; i < widget._assetList.length; ++i) {
-      if (i != ix) {
-        images.add(Image.asset(
-          widget._assetList[i],
-          width: 0,
-          height: 0,
-        ));
-      }
-    }
 
-    images.add(Image.asset(
-      widget._assetList[ix],
-      width: widget.width,
-      height: widget.height,
-    ));
+    if (_status == AnimationStatus.completed) {
+      images.add(
+        Image.asset(
+          widget._assetList.last,
+          width: widget.width,
+          height: widget.height,
+        ),
+      );
+    } else {
+      // 把所有图片都加载进内容，否则每一帧加载时会卡顿
+      for (int i = 0; i < widget._assetList.length; ++i) {
+        if (i != ix) {
+          images.add(
+            Image.asset(
+              widget._assetList[i],
+              width: 0,
+              height: 0,
+            ),
+          );
+        }
+      }
+
+      images.add(
+        Image.asset(
+          widget._assetList[ix],
+          width: widget.width,
+          height: widget.height,
+        ),
+      );
+    }
 
     return Stack(alignment: AlignmentDirectional.center, children: images);
   }
